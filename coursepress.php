efae269ee4ec8a310599408e1b5b74fdf6f6ff62
Marko Miljus <marko@incsub.com>
2014-09-08 11:19:43 +0200

    Changing version and name...again :)

--- coursepress.php
+++ coursepress.php
@@ -6,7 +6,7 @@
   Author: WPMU DEV
   Author URI: http://premium.wpmudev.org
   Developers: Marko Miljus ( https://twitter.com/markomiljus ), Rheinard Korf ( https://twitter.com/rheinardkorf )
-  Version: 1.0.3b
+  Version: 1.0.0
   TextDomain: cp
   Domain Path: /languages/
   WDP ID: 896496
