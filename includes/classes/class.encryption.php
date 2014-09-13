719b1818457d78c1915cf925acc45c8660f2edc0
Marko Miljus <marko@incsub.com>
2014-09-13 12:52:50 +0200

    Resolved potential issue if Mcrypt library is not installed on server

--- includes/classes/class.encryption.php
+++ includes/classes/class.encryption.php
@@ -5,44 +5,50 @@ class CP_Encryption {
     var $security_key = NONCE_KEY;
 
     function safe_b64encode( $string ) {
-        $data = base64_encode( $string );
-        $data = str_replace( array( '+', '/', '=' ), array( '-', '_', '' ), $data );
+        $data = base64_encode($string);
+        $data = str_replace(array( '+', '/', '=' ), array( '-', '_', '' ), $data);
         return $data;
     }
 
     function safe_b64decode( $string ) {
-        $data = str_replace( array( '-', '_' ), array( '+', '/' ), $string );
-        $mod4 = strlen( $data ) % 4;
+        $data = str_replace(array( '-', '_' ), array( '+', '/' ), $string);
+        $mod4 = strlen($data) % 4;
         if ( $mod4 ) {
-            $data .= substr( '====', $mod4 );
+            $data .= substr('====', $mod4);
         }
-        return base64_decode( $data );
+        return base64_decode($data);
     }
 
     function encode( $value ) {
+        if ( extension_loaded('mcrypt') && function_exists('mcrypt_module_open') ) {
+            if ( !$value ) {
+                return false;
+            }
 
-        if ( !$value ) {
-            return false;
+            $text = $value;
+            $iv_size = mcrypt_get_iv_size(MCRYPT_RIJNDAEL_256, MCRYPT_MODE_ECB);
+            $iv = mcrypt_create_iv($iv_size, MCRYPT_RAND);
+            $crypttext = mcrypt_encrypt(MCRYPT_RIJNDAEL_256, mb_substr($this->security_key, 0, 20), $text, MCRYPT_MODE_ECB, $iv);
+            return trim($this->safe_b64encode($crypttext));
+        }else{
+            return $value;
         }
-
-        $text = $value;
-        $iv_size = mcrypt_get_iv_size( MCRYPT_RIJNDAEL_256, MCRYPT_MODE_ECB );
-        $iv = mcrypt_create_iv( $iv_size, MCRYPT_RAND );
-        $crypttext = mcrypt_encrypt( MCRYPT_RIJNDAEL_256, mb_substr( $this->security_key, 0, 20 ), $text, MCRYPT_MODE_ECB, $iv );
-        return trim( $this->safe_b64encode( $crypttext ) );
     }
 
     function decode( $value ) {
+        if ( extension_loaded('mcrypt') && function_exists('mcrypt_module_open') ) {
+            if ( !$value ) {
+                return false;
+            }
 
-        if ( !$value ) {
-            return false;
+            $crypttext = $this->safe_b64decode($value);
+            $iv_size = mcrypt_get_iv_size(MCRYPT_RIJNDAEL_256, MCRYPT_MODE_ECB);
+            $iv = mcrypt_create_iv($iv_size, MCRYPT_RAND);
+            $decrypttext = mcrypt_decrypt(MCRYPT_RIJNDAEL_256, mb_substr($this->security_key, 0, 20), $crypttext, MCRYPT_MODE_ECB, $iv);
+            return trim($decrypttext);
+        }else{
+            return $value;
         }
-
-        $crypttext = $this->safe_b64decode( $value );
-        $iv_size = mcrypt_get_iv_size( MCRYPT_RIJNDAEL_256, MCRYPT_MODE_ECB );
-        $iv = mcrypt_create_iv( $iv_size, MCRYPT_RAND );
-        $decrypttext = mcrypt_decrypt( MCRYPT_RIJNDAEL_256, mb_substr( $this->security_key, 0, 20 ), $crypttext, MCRYPT_MODE_ECB, $iv );
-        return trim( $decrypttext );
     }
 
 }
