74a8703f2f855177de7ac74d136b1e376408ba04
Marko Miljus <marko@incsub.com>
2014-09-19 13:33:42 +0200

    Resolved issue with non-protected discussions for students who didn't enroll to the course

--- includes/templates/single-discussion-before-details.php
+++ includes/templates/single-discussion-before-details.php
@@ -1,5 +1,8 @@
 <?php
-global $post;
+global $post, $coursepress;
 $course_id = get_post_meta($post->ID, 'course_id', true);
+//redirect to the parent course page if not enrolled
+$coursepress->check_access($course_id);
+
 do_shortcode('[course_unit_archive_submenu course_id="'.$course_id.'"]');
 ?>
