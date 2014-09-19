cd912b9aa9a9105e33290545c8335270f7257354
Marko Miljus <marko@incsub.com>
2014-09-19 17:22:05 +0200

    * Resolved issue with details button on courses archive and inconsistent shortcode used

--- coursepress.php
+++ coursepress.php
@@ -6,7 +6,7 @@
   Author: WPMU DEV
   Author URI: http://premium.wpmudev.org
   Developers: Marko Miljus ( https://twitter.com/markomiljus ), Rheinard Korf ( https://twitter.com/rheinardkorf )
-  Version: 1.0.8
+  Version: 1.0.9
   TextDomain: cp
   Domain Path: /languages/
   WDP ID: 913071
