eb5e11c7e81c1c08c0c192e8e678b0dfbc28607d
Rheinard Korf <rheinard@thekorfs.com>
2014-10-28 15:16:46 +1100

    Versioning and Changelog.

--- coursepress.php
+++ coursepress.php
@@ -6,7 +6,7 @@
   Author: WPMU DEV
   Author URI: http://premium.wpmudev.org
   Developers: Marko Miljus ( https://twitter.com/markomiljus ), Rheinard Korf ( https://twitter.com/rheinardkorf )
-  Version: 1.2.2.1
+  Version: 1.2.2.2
   TextDomain: cp
   Domain Path: /languages/
   WDP ID: 913071
@@ -62,7 +62,7 @@ if ( !class_exists( 'CoursePress' ) ) {
 		 * @since 1.0.0
 		 * @var string
 		 */
-		public $version = '1.2.2.1';
+		public $version = '1.2.2.2';
 
 		/**
 		 * Plugin friendly name.
