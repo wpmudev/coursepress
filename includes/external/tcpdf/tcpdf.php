4109b00fe7b29de3355fa59cf8d65f07a1dc7bae
Marko Miljus <marko@incsub.com>
2014-10-29 14:05:56 +0100

    Resolved issues with UTF-8 characters in filename in the TCPDF library

--- includes/external/tcpdf/tcpdf.php
+++ includes/external/tcpdf/tcpdf.php
@@ -8967,8 +8967,9 @@ class TCPDF {
 		}
 		$dest = strtoupper($dest);
 		if ($dest{0} != 'F') {
-			$name = preg_replace('/[\s]+/', '_', $name);
-			$name = preg_replace('/[^a-zA-Z0-9_\.-]/', '', $name);
+			//$name = preg_replace('/[\s]+/', '_', $name);
+			//$name = preg_replace('/[^a-zA-Z0-9_\.-]/', '', $name);
+			$name = strtolower(str_replace(' ', '_', $name));
 		}
 		if ($this->sign) {
 			// *** apply digital signature to the document ***
