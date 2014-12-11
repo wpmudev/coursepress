011817f4c1093b084849b6eb66c40fd8f9ec5b7b
Rheinard Korf <rheinard@thekorfs.com>
2014-12-11 15:19:23 +1100

    Adding class_exist() for certificate elements
    Odd case where classes are redeclared causing CoursePress to crash.
    
    Note: Still need to find why its getting called a second time.

--- includes/certificate-elements/website.php
+++ includes/certificate-elements/website.php
@@ -1,18 +1,20 @@
 <?php
 
-class cp_website_element extends CP_Certificate_Template_Elements {
+if( ! class_exists( 'cp_website_element' ) ) {
+	class cp_website_element extends CP_Certificate_Template_Elements {
 
-	var $element_name	 = 'cp_website_element';
-	var $element_title	 = '';
+		var $element_name = 'cp_website_element';
+		var $element_title = '';
 
-	function on_creation() {
-		$this->element_title = apply_filters( 'coursepress_website_element_title', __( 'Website', 'cp' ) );
+		function on_creation() {
+			$this->element_title = apply_filters( 'coursepress_website_element_title', __( 'Website', 'cp' ) );
+		}
+
+		function template_content( $course_id = false, $user_id = false, $preview = false ) {
+
+		}
+
 	}
 
-	function template_content( $course_id = false, $user_id = false, $preview = false  ) {
-
-	}
-
+	cp_register_template_element( 'cp_website_element', __( 'Website', 'cp' ) );
 }
-
-cp_register_template_element( 'cp_website_element', __( 'Website', 'cp' ) );
