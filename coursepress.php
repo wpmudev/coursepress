565c54a8e58d511d37191f2b902159f2d044480b
Marko Miljus <marko@incsub.com>
2015-01-06 10:59:07 +0100

    Changed copyright year

--- coursepress.php
+++ coursepress.php
@@ -12,7 +12,7 @@
   WDP ID: 913071
   License: GNU General Public License ( Version 2 - GPLv2 )
 
-  Copyright 2014 Incsub ( http://incsub.com )
+  Copyright 2015 Incsub ( http://incsub.com )
 
   This program is free software; you can redistribute it and/or modify
   it under the terms of the GNU General Public License ( Version 2 - GPLv2 ) as published by
