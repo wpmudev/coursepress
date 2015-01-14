88c7fa5a8b543e279e2643347ce95620ad7dd1f9
Marko Miljus <marko@incsub.com>
2015-01-14 12:47:55 +0100

    Changed log and version

--- coursepress.php
+++ coursepress.php
@@ -6,7 +6,7 @@
   Author: WPMU DEV
   Author URI: http://premium.wpmudev.org
   Developers: Marko Miljus ( https://twitter.com/markomiljus ), Rheinard Korf ( https://twitter.com/rheinardkorf )
-  Version: 1.2.4
+  Version: 1.2.4.1
   TextDomain: cp
   Domain Path: /languages/
   WDP ID: 913071
@@ -64,7 +64,7 @@ if ( !class_exists( 'CoursePress' ) ) {
 		 * @since 1.0.0
 		 * @var string
 		 */
-		public $version = '1.2.4';
+		public $version = '1.2.4.1';
 
 		/**
 		 * Plugin friendly name.
