6ae1e7b1d8ea74096b70378002124a551b7511e3
Philipp Stracker <stracker.phil@gmail.com>
2016-02-10 21:50:00 +0100

    First test commit. Just change the indentation in grunt files from space to tab

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,11 +1,11 @@
 module.exports = function(grunt) {
-  
+
 	// Project configuration.
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
 		makepot: {
-		    target: {
-		        options: {
+			target: {
+				options: {
 					domainPath: '/languages',
 					mainFile: 'coursepress.php',
 					potFilename: 'cp-default.pot',
@@ -16,44 +16,43 @@ module.exports = function(grunt) {
 						'last-translator': 'WPMU Dev <support@wpmudev.org>',
 						'x-generator': 'grunt-wp-i18n'
 					},
-		            type: 'wp-plugin'
-		        }
-		    }
+					type: 'wp-plugin'
+				}
+			}
 		},
 		wpmu_pot2mo: {
-		    files: {
-		        src: 'languages/*.pot',
-		        expand: true,
-		    },
+			files: {
+				src: 'languages/*.pot',
+				expand: true,
+			},
 		},
 	});
 
 	// Load wp-i18n
 	grunt.loadNpmTasks( 'grunt-wp-i18n' );
-	
+
 
 	// Adapted from https://github.com/MicheleBertoli/grunt-po2mo
 	grunt.registerMultiTask('wpmu_pot2mo', 'Compile .pot files into binary .mo files with msgfmt.', function() {
 		this.files.forEach(function(file) {
 
-		  var dest = file.dest;
-		  if (dest.indexOf('.pot') > -1) {
-		      dest = dest.replace('.pot', '.mo');
-		  }
-		  grunt.file.write(dest);
+			var dest = file.dest;
+			if (dest.indexOf('.pot') > -1) {
+				dest = dest.replace('.pot', '.mo');
+			}
+			grunt.file.write(dest);
 
-		  var exec = require('child_process').exec;
-		  var command = 'msgfmt -o ' + dest + ' ' + file.src[0];
+			var exec = require('child_process').exec;
+			var command = 'msgfmt -o ' + dest + ' ' + file.src[0];
 
-		  grunt.verbose.writeln('Executing: ' + command);
-		  exec(command);
+			grunt.verbose.writeln('Executing: ' + command);
+			exec(command);
 
 		});
 	});
-	
-	
+
+
 	// Default task(s).
 	grunt.registerTask( 'default', ['makepot', 'wpmu_pot2mo'] );
-	
 
 };
