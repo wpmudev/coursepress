5f863f9067b645c1bb4dee825bc29ce2f0307b8c
Philipp Stracker <stracker.phil@gmail.com>
2016-02-17 19:24:18 +0100

    Remove most of the old files
    Some files that are useful to review during further 2.0 review are moved to
    folder /deprecated.

--- includes/uninstall.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-// Removed code as it was too resource heavy (even though it was already commented out.)
-
-?>
--- js/chosen-config.js
+++
@@ -1,8 +0,0 @@
-var config = {
-    '.chosen-select': {},
-    '.chosen-select-student': {width:"20%"},
-    '.chosen-select-course': { width:"61.5%"},
-}
-for (var selector in config) {
-    jQuery(selector).chosen(config[selector]);
-}
