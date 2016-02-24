6af29a38128022bc5b5e2ba958b662fa25779710
Philipp Stracker <philipp@incsub.com>
2016-02-24 18:45:31 +0100

    Fix fatal error during registration of invited instructor
    Error caused by a typo in WP function name.

--- includes/templates/student-signup.php
+++ includes/templates/student-signup.php
@@ -1,6 +1,6 @@
 <?php
 $redirect_url = '';
 if ( ! empty( $_REQUEST['redirect_url'] ) ) {
-	$redirect_url = esc_raw_url( urldecode( $_REQUEST['redirect_url'] ) );
+	$redirect_url = esc_url_raw( urldecode( $_REQUEST['redirect_url'] ) );
 }
 echo do_shortcode( '[course_signup page="signup" signup_title="" redirect_url="' . $redirect_url . '" login_url="' . CoursePress::instance()->get_login_slug( true ) . '"]' );
