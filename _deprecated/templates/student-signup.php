4905d025a14e8d2531a7d02cf21f545f40f2d9a1
Marcin Pietrzak <marcin@iworks.pl>
2016-02-25 21:39:43 +0100

    [2.0] Convert the [cp-pages] shortcode https://www.meistertask.com/app/task/A8BluIMY/2-0-convert-the-cp-pages-shortcode

--- _deprecated/templates/student-signup.php
+++
@@ -1,6 +0,0 @@
-<?php
-$redirect_url = '';
-if ( ! empty( $_REQUEST['redirect_url'] ) ) {
-	$redirect_url = $_REQUEST['redirect_url'];
-}
-echo do_shortcode( '[course_signup page="signup" signup_title="" redirect_url="' . $redirect_url . '" login_url="' . CoursePress::instance()->get_login_slug( true ) . '"]' );
