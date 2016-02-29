0ae7d1e2f9fe1a078a16da949d7dd792f67acdbc
Ignacio Cruz <igmoweb@gmail.com>
2016-02-29 16:28:48 +0100

    New release, 1.3

--- coursepress.php
+++ coursepress.php
@@ -12,7 +12,7 @@ Domain Path: /languages/
 WDP ID: 913071
 License: GNU General Public License ( Version 2 - GPLv2 )
 
-Copyright 2015 Incsub ( http://incsub.com )
+Copyright 2016 Incsub ( http://incsub.com )
 
 This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License ( Version 2 - GPLv2 ) as published by
