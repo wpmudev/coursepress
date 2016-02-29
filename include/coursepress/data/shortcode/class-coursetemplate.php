a743daf5f83d20316f317b096ad3f8255d93b2be
Marcin Pietrzak <marcin@iworks.pl>
2016-02-29 20:30:16 +0100

    [2.0] Find missing shortcodes https://www.meistertask.com/app/task/3UraFgIq/2-0-find-missing-shortcodes Fixed file formating.

--- include/coursepress/data/shortcode/class-coursetemplate.php
+++ include/coursepress/data/shortcode/class-coursetemplate.php
@@ -964,14 +964,13 @@ class CoursePress_Data_Shortcode_CourseTemplate {
 			$args = array( 'course_id' => $course_id );
 		}
 
-        $args['date_indicator'] = $date_indicator;
+		$args['date_indicator'] = $date_indicator;
 
+		return CoursePress_Data_Calendar::get_calendar( $args, $pre, $next );
 
-        return CoursePress_Data_Calendar::get_calendar( $args, $pre, $next );
+		//      $cal = new Course_Calendar( $args ); // @check
 
-//		$cal = new Course_Calendar( $args ); // @check
-
-//		return $cal->create_calendar( $pre, $next );
+		//      return $cal->create_calendar( $pre, $next );
 	}
 
 	/**
