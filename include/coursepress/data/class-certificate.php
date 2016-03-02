027e801f13daee95af7820ff21e8974a202c5627
Philipp Stracker <philipp@incsub.com>
2016-03-02 20:44:07 +0100

    Add missing function, that is used in Data_Student class
    Also fix the parameters for the email-function call.

--- include/coursepress/data/class-certificate.php
+++ include/coursepress/data/class-certificate.php
@@ -63,9 +63,23 @@ class CoursePress_Data_Certificate {
 	}
 
 	/**
+	 * Generate the certificate, store it in DB and send email to the student.
+	 *
+	 * @since  2.0.0
+	 * @param  int $student_id The WP user-ID.
+	 * @param  int $course_id The course-ID that was completed.
+	 */
+	public function generate_certificate( $student_id, $course_id ) {
+		self::send_certificate( $student_id, $course_id );
+	}
+
+	/**
 	 * Send certificate to student.
 	 *
 	 * @since 2.0.0
+	 * @param  int $student_id The WP user-ID.
+	 * @param  int $course_id The course-ID that was completed.
+	 * @return bool True on success.
 	 */
 	public function send_certificate( $student_id, $course_id ) {
 		// If student doesn't exist, exit.
@@ -75,12 +89,14 @@ class CoursePress_Data_Certificate {
 		}
 
 		$email_args = array();
-		$email_args['email_type'] = CoursePress_Helper_Email::BASIC_CERTIFICATE;
 		$email_args['course_id'] = $course_id;
 		$email_args['email'] = sanitize_email( $student->user_email );
 		$email_args['first_name'] = $student->user_firstname;
 		$email_args['last_name'] = $student->user_lastname;
 
-		return CoursePress_Helper_Email::send_email( $email_args );
+		return CoursePress_Helper_Email::send_email(
+			CoursePress_Helper_Email::BASIC_CERTIFICATE,
+			$email_args
+		);
 	}
 }
