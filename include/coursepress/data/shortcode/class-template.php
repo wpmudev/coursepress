8a984602dc4a3367f0978e7019a89296a184f26d
Marcin Pietrzak <marcin@iworks.pl>
2016-04-06 08:46:58 +0200

    [2.0] Dump VirtualPages! https://www.meistertask.com/app/task/ux7T61WQ/2-0-dump-virtualpages - login on main - login on popup

--- include/coursepress/data/shortcode/class-template.php
+++ include/coursepress/data/shortcode/class-template.php
@@ -13,6 +13,8 @@
  */
 class CoursePress_Data_Shortcode_Template {
 
+	static private $show_error = false;
+
 	/**
 	 * Register the shortcodes.
 	 *
@@ -71,6 +73,33 @@ class CoursePress_Data_Shortcode_Template {
 			'messaging_submenu',
 			array( __CLASS__, 'messaging_submenu' )
 		);
+
+		add_action(
+			'parse_request',
+			array( __CLASS__, 'try_to_login' )
+		);
+
+	}
+
+	public static function try_to_login() {
+		if ( isset( $_POST['log'] ) && isset( $_POST['pwd'] ) ) {
+
+			$auth = wp_authenticate_username_password( null, $_POST['log'], $_POST['pwd'] );
+			if ( ! is_wp_error( $auth ) ) {
+				$user = get_user_by( 'login', $_POST['log'] );
+				$user_id = $user->ID;
+				wp_set_current_user( $user_id );
+				wp_set_auth_cookie( $user_id );
+				if ( ! empty( $redirect_url ) ) {
+					wp_redirect( urldecode( esc_url_raw( $redirect_url ) ) );
+				} else {
+					wp_redirect( esc_url_raw( CoursePress_Core::get_slug( 'student_dashboard', true ) ) );
+				}
+				exit;
+			} else {
+				self::$show_error = true;
+			}
+		}
 	}
 
 	public static function course_archive( $a ) {
@@ -899,34 +928,15 @@ class CoursePress_Data_Shortcode_Template {
 			$login_url = $login_url . '?redirect_url=' . $_POST['redirect_url'];
 		}
 
-		// Set a cookie now to see if they are supported by the browser.
-		setcookie( TEST_COOKIE, 'WP Cookie check', 0, COOKIEPATH, COOKIE_DOMAIN );
-		if ( SITECOOKIEPATH != COOKIEPATH ) {
-			setcookie( TEST_COOKIE, 'WP Cookie check', 0, SITECOOKIEPATH, COOKIE_DOMAIN );
-		};
-
 		$form_message = '';
 		$form_message_class = '';
 
-		// Attempt a login if submitted.
-		if ( isset( $_POST['log'] ) && isset( $_POST['pwd'] ) ) {
-
-			$auth = wp_authenticate_username_password( null, $_POST['log'], $_POST['pwd'] );
-			if ( ! is_wp_error( $auth ) ) {
-				$user = get_user_by( 'login', $_POST['log'] );
-				$user_id = $user->ID;
-				wp_set_current_user( $user_id );
-				wp_set_auth_cookie( $user_id );
-				if ( ! empty( $redirect_url ) ) {
-					wp_redirect( urldecode( esc_url_raw( $redirect_url ) ) );
-				} else {
-					wp_redirect( esc_url_raw( CoursePress_Core::get_slug( 'student_dashboard', true ) ) );
-				}
-				exit;
-			} else {
-				$form_message = $failed_login_text;
-				$form_message_class = $failed_login_class;
-			}
+		/**
+		 * Set error messages if login fails. See method try_to_login()
+		 */
+		if ( isset( $_POST['log'] ) && isset( $_POST['pwd'] ) && self::$show_error ) {
+			$form_message = $failed_login_text;
+			$form_message_class = $failed_login_class;
 		}
 
 		$content = '';
