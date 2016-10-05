7a6d865ee8928b1a7ea2abbee9c61664c2b3d477
Marcin Pietrzak <marcin@iworks.pl>
2016-10-05 10:23:01 +0200

    Change cap "Update own courses" to "Update own published notification".

--- include/coursepress/view/admin/setting/class-capabilities.php
+++ include/coursepress/view/admin/setting/class-capabilities.php
@@ -236,7 +236,7 @@ class CoursePress_View_Admin_Setting_Capabilities {
 		return array(
 			'coursepress_create_my_notification_cap' => __( 'Create new notifications for own courses', 'cp' ),
 			'coursepress_create_my_assigned_notification_cap' => __( 'Create new notifications for assigned courses', 'cp' ),
-			'coursepress_update_my_notification_cap' => __( 'Update own notifications', 'cp' ),
+			'coursepress_update_my_notification_cap' => __( 'Update own published notification', 'cp' ),
 			'coursepress_update_notification_cap' => __( 'Update every notification', 'cp' ),
 			'coursepress_delete_my_notification_cap' => __( 'Delete own notifications', 'cp' ),
 			'coursepress_delete_notification_cap' => __( 'Delete every notification', 'cp' ),
