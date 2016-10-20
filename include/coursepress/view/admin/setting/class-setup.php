a757cc2ac4f44aa454c40311fffdfa96acac25d6
diobeth <diobethrayvilla@gmail.com>
2016-10-20 12:18:17 +0800

    Fixed Change or set the correct url at Setup -> start building button (Course Settings)

--- include/coursepress/view/admin/setting/class-setup.php
+++ include/coursepress/view/admin/setting/class-setup.php
@@ -173,7 +173,7 @@ if ( current_user_can( 'manage_options' ) && ! get_option( 'permalink_structure'
 <?php
 	}
 } else {
-	$url = add_query_arg( 'page', CoursePress_View_Admin_Course_Edit::$slug, admin_url( 'admin.php' ) );
+	$url = admin_url('post-new.php?post_type=' . CoursePress_Data_Course::get_post_type_name());
 ?>
 	<a href="<?php echo esc_url( $url ); ?>" class="button button-units save-unit-button start-course-button"><?php _e( 'Start building your own course now &rarr;', 'cp' ); ?></a>
 <?php
