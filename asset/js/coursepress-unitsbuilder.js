33bee80ad1c0a94e1779573bd09cd4f6e815c5d7
Irene <webdevenquiry@gmail.com>
2016-10-24 23:31:57 +0800

    Fix input-upload validation

--- asset/js/coursepress-unitsbuilder.js
+++ asset/js/coursepress-unitsbuilder.js
@@ -1003,15 +1003,8 @@ var CoursePress = CoursePress || {};
 		}
 	}
 
-	CoursePress.Helpers.Module.toggle_unit_state = function( e ) {
-		var nonce = $( '#unit-builder' ).attr( 'data-nonce' ),
-			parent_container = $(e.currentTarget).closest('.unit-builder-content'),
-			header_buttons = parent_container.find('.unit-builder-header .unit-buttons'),
-			footer_buttons = parent_container.find('.unit-builder-footer .unit-buttons'),
-			loading_mask = '<div class="unit-loading-mask"><i class="fa fa-spinner fa-spin save-progress"></i></div>',
-			unit_modules = parent_container.find('.unit-builder-components')
-			
-		;
+	CoursePress.Helpers.Module.toggle_unit_state = function() {
+		var nonce = $( '#unit-builder' ).attr( 'data-nonce' );
 
 		this.switch = function( state, unit_id, unit_ref ) {
 			if ( 'publish' === state ) {
@@ -1049,13 +1042,6 @@ var CoursePress = CoursePress || {};
 		var unit_ref = CoursePress.UnitBuilder.activeUnitRef;
 		var state = CoursePress.UnitBuilder.unit_collection._byId[ unit_ref ].get( 'post_status' );
 		state = 'publish' === state ? 'draft' : 'publish';
-		
-		// Improved Flow: Hide Unit buttons/modules and show Loading instead
-		if ( !header_buttons.prev().hasClass('unit-loading-mask') ) $(loading_mask).insertBefore(header_buttons);
-		header_buttons.hide();
-		if ( !footer_buttons.prev().hasClass('unit-loading-mask') ) $(loading_mask).insertBefore(footer_buttons);
-		footer_buttons.hide();
-		unit_modules.hide();
 
 		CoursePress.UnitBuilder.unit_collection.url = _coursepress._ajax_url + '?action=unit_builder&task=unit_toggle&course_id=' + _coursepress.course_id + '&wp_nonce=' + nonce + '&state=' + state + '&unit_id=' + unit_id;
 		Backbone.sync( 'update', CoursePress.UnitBuilder.unit_collection, {
@@ -1068,16 +1054,7 @@ var CoursePress = CoursePress || {};
 				self.switch( response[ 'post_status' ], unit_id, unit_ref );
 				$( '#unit-builder' ).attr( 'data-nonce', response[ 'nonce' ] );
 			}
-		} ).done( function() {
-			var header_prev = header_buttons.prev(),
-				footer_prev = footer_buttons.prev()
-			;
-			if ( header_prev.hasClass('unit-loading-mask') ) header_prev.remove();
-			header_buttons.show();
-			if ( footer_prev.hasClass('unit-loading-mask') ) footer_prev.remove();
-			footer_buttons.show();
-			unit_modules.show();
-		});
+		} );
 
 		// Reset URL
 		CoursePress.UnitBuilder.unit_collection.url = _coursepress._ajax_url + '?action=unit_builder&task=units&course_id=' + _coursepress.course_id;
--- asset/js/src/front-modules.js
+++ asset/js/src/front-modules.js
@@ -55,6 +55,8 @@
 			iframe = false,
 			timer = false,
 			module_elements = $( '.module-elements[data-required="1"]', form ),
+			module_response = module_elements.next( '.module-response' ),
+			is_focus = form.parents( '.coursepress-focus-view' ).length > 0,
 			error = 0, mask
 		;
 
@@ -75,8 +77,13 @@
 				if ( 0 == input.length ) {
 					error += 1;
 				}
+			} else if ( 'input-upload' === module_type && 0 === module_response.length ) {
+				input = $( '[type="file"]', module );
+				if ( '' === input.val() ) {
+					error += 1;
+				}
 			// Validate input module
-			} else if ( _.contains( ['input-upload', 'input-text', 'input-textarea', 'input-select'], module_type ) ) {
+			} else if ( _.contains( ['input-text', 'input-textarea', 'input-select'], module_type ) ) {
 				input = $( 'input,textarea,select', module );
 				if ( '' === input.val() ) {
 					error += 1;
@@ -87,7 +94,7 @@
 		if ( error > 0 ) {
 			// Don't submit if an error is found!
 			new CoursePress.WindowAlert({
-				message: _coursepress.module_error.required
+				message: _coursepress.module_error[ is_focus ? 'required' : 'normal_required' ]
 			});
 
 			return false;
@@ -123,7 +130,7 @@
 					if ( true === data.success ) {
 						// Process success
 						if ( data.data.url ) {
-							if ( data.data.type && 'completion' === data.data.type ) {
+							if ( false === is_focus || data.data.type && 'completion' === data.data.type ) {
 								window.location = data.data.url;
 							} else {
 								focus_box.html( data.data.html );
@@ -138,8 +145,6 @@
 						new CoursePress.WindowAlert({
 							message: data.data.error_message
 						});
-						//error_box = $( '.cp-form' );
-						//CoursePress.showError( data.data.error_message, error_box );
 					}
 				}
 			}, 100 );
