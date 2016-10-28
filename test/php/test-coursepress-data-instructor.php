cff2b972a7ee398bb84f63f4652e86ad0cf8b021
Marcin Pietrzak <marcin@iworks.pl>
2016-10-28 18:36:18 +0200

    more tests

--- test/php/test-coursepress-data-instructor.php
+++ test/php/test-coursepress-data-instructor.php
@@ -4,65 +4,102 @@
  */
 class CoursepressDataInstructorTest extends WP_UnitTestCase {
 
-    protected $instructor;
+	protected $instructor;
 
-    public function __construct() {
-        $userdata = array(
-            'user_login'  => 'instructor',
-            'user_url'    => 'https://premium.wpmudev.org/',
-            'user_pass'   => 'instructor',
-            'first_name'  => 'Jon',
-            'last_name'   => 'Snow',
-            'nickname'    => 'bastard',
-            'description' => 'Winter is comming.',
-        );
-        $user_id = wp_insert_user( $userdata );
-        $this->instructor = get_userdata( $user_id );
-    }
-
-    public function test_instructor() {
-        $this->assertInstanceOf( 'WP_User', $this->instructor );
-    }
+	public function __construct() {
+		/**
+		 * Set instructor data
+		 */
+		$this->instructor = get_user_by( 'login', 'instructor' );
+		if ( false === $this->instructor ) {
+			$userdata = array(
+				'user_login'  => 'instructor',
+				'user_url'    => 'https://premium.wpmudev.org/',
+				'user_pass'   => 'instructor',
+				'first_name'  => 'Jon',
+				'last_name'   => 'Snow',
+				'nickname'    => 'bastard',
+				'description' => 'Winter is comming.',
+			);
+			$user_id = wp_insert_user( $userdata );
+			$this->instructor = get_userdata( $user_id );
+		}
+	}
 
 	public function test_exists() {
 		$this->assertTrue( class_exists( 'CoursePress_Data_Instructor' ) );
-        $this->assertTrue(is_callable(array( 'CoursePress_Data_Instructor', 'get_first_name')));
-        $this->assertTrue(is_callable(array( 'CoursePress_Data_Instructor', 'get_last_name')));
-        $this->assertTrue(is_callable(array( 'CoursePress_Data_Instructor', 'get_course_count')));
-        $this->assertTrue(is_callable(array( 'CoursePress_Data_Instructor', 'get_course_meta_keys')));
-        $this->assertTrue(is_callable(array( 'CoursePress_Data_Instructor', 'filter_course_meta_array')));
-        $this->assertTrue(is_callable(array( 'CoursePress_Data_Instructor', 'filter_by_where')));
-        $this->assertTrue(is_callable(array( 'CoursePress_Data_Instructor', 'filter_by_whereall')));
-        $this->assertTrue(is_callable(array( 'CoursePress_Data_Instructor', 'get_assigned_courses_ids')));
-        $this->assertTrue(is_callable(array( 'CoursePress_Data_Instructor', 'get_accessable_courses')));
-        $this->assertTrue(is_callable(array( 'CoursePress_Data_Instructor', 'unassign_from_course')));
-        $this->assertTrue(is_callable(array( 'CoursePress_Data_Instructor', 'unassign_from_all_courses')));
-        $this->assertTrue(is_callable(array( 'CoursePress_Data_Instructor', 'get_courses_number')));
-        $this->assertTrue(is_callable(array( 'CoursePress_Data_Instructor', 'is_assigned_to_course')));
-        $this->assertTrue(is_callable(array( 'CoursePress_Data_Instructor', 'remove_instructor_status')));
-        $this->assertTrue(is_callable(array( 'CoursePress_Data_Instructor', 'delete_instructor')));
-        $this->assertTrue(is_callable(array( 'CoursePress_Data_Instructor', 'instructor_by_hash')));
-        $this->assertTrue(is_callable(array( 'CoursePress_Data_Instructor', 'instructor_by_login')));
-        $this->assertTrue(is_callable(array( 'CoursePress_Data_Instructor', 'create_hash')));
-        $this->assertTrue(is_callable(array( 'CoursePress_Data_Instructor', 'get_hash')));
-        $this->assertTrue(is_callable(array( 'CoursePress_Data_Instructor', 'count_courses')));
-        $this->assertTrue(is_callable(array( 'CoursePress_Data_Instructor', 'meta_key')));
-        $this->assertTrue(is_callable(array( 'CoursePress_Data_Instructor', 'instructor_key')));
-        $this->assertTrue(is_callable(array( 'CoursePress_Data_Instructor', 'added_to_course')));
-        $this->assertTrue(is_callable(array( 'CoursePress_Data_Instructor', 'remove_from_all_courses')));
-        $this->assertTrue(is_callable(array( 'CoursePress_Data_Instructor', 'removed_from_course')));
-        $this->assertTrue(is_callable(array( 'CoursePress_Data_Instructor', 'delete_invitation')));
-        $this->assertTrue(is_callable(array( 'CoursePress_Data_Instructor', 'send_invitation')));
-        $this->assertTrue(is_callable(array( 'CoursePress_Data_Instructor', 'create_invite_code_hash')));
-        $this->assertTrue(is_callable(array( 'CoursePress_Data_Instructor', 'is_course_invite')));
-        $this->assertTrue(is_callable(array( 'CoursePress_Data_Instructor', 'invitation_data')));
-        $this->assertTrue(is_callable(array( 'CoursePress_Data_Instructor', 'verify_invitation_code')));
-        $this->assertTrue(is_callable(array( 'CoursePress_Data_Instructor', 'add_from_invitation')));
-        $this->assertTrue(is_callable(array( 'CoursePress_Data_Instructor', 'get_students_count')));
-        $this->assertTrue(is_callable(array( 'CoursePress_Data_Instructor', '_get_students_count')));
-        $this->assertTrue(is_callable(array( 'CoursePress_Data_Instructor', 'reset_students_count')));
-        $this->assertTrue(is_callable(array( 'CoursePress_Data_Instructor', 'get_nonce_action')));
+		$this->assertTrue( is_callable( array( 'CoursePress_Data_Instructor', 'get_first_name' ) ) );
+		$this->assertTrue( is_callable( array( 'CoursePress_Data_Instructor', 'get_last_name' ) ) );
+		$this->assertTrue( is_callable( array( 'CoursePress_Data_Instructor', 'get_course_count' ) ) );
+		$this->assertTrue( is_callable( array( 'CoursePress_Data_Instructor', 'get_course_meta_keys' ) ) );
+		$this->assertTrue( is_callable( array( 'CoursePress_Data_Instructor', 'filter_course_meta_array' ) ) );
+		$this->assertTrue( is_callable( array( 'CoursePress_Data_Instructor', 'filter_by_where' ) ) );
+		$this->assertTrue( is_callable( array( 'CoursePress_Data_Instructor', 'filter_by_whereall' ) ) );
+		$this->assertTrue( is_callable( array( 'CoursePress_Data_Instructor', 'get_assigned_courses_ids' ) ) );
+		$this->assertTrue( is_callable( array( 'CoursePress_Data_Instructor', 'get_accessable_courses' ) ) );
+		$this->assertTrue( is_callable( array( 'CoursePress_Data_Instructor', 'unassign_from_course' ) ) );
+		$this->assertTrue( is_callable( array( 'CoursePress_Data_Instructor', 'unassign_from_all_courses' ) ) );
+		$this->assertTrue( is_callable( array( 'CoursePress_Data_Instructor', 'get_courses_number' ) ) );
+		$this->assertTrue( is_callable( array( 'CoursePress_Data_Instructor', 'is_assigned_to_course' ) ) );
+		$this->assertTrue( is_callable( array( 'CoursePress_Data_Instructor', 'remove_instructor_status' ) ) );
+		$this->assertTrue( is_callable( array( 'CoursePress_Data_Instructor', 'delete_instructor' ) ) );
+		$this->assertTrue( is_callable( array( 'CoursePress_Data_Instructor', 'instructor_by_hash' ) ) );
+		$this->assertTrue( is_callable( array( 'CoursePress_Data_Instructor', 'instructor_by_login' ) ) );
+		$this->assertTrue( is_callable( array( 'CoursePress_Data_Instructor', 'create_hash' ) ) );
+		$this->assertTrue( is_callable( array( 'CoursePress_Data_Instructor', 'get_hash' ) ) );
+		$this->assertTrue( is_callable( array( 'CoursePress_Data_Instructor', 'count_courses' ) ) );
+		$this->assertTrue( is_callable( array( 'CoursePress_Data_Instructor', 'meta_key' ) ) );
+		$this->assertTrue( is_callable( array( 'CoursePress_Data_Instructor', 'instructor_key' ) ) );
+		$this->assertTrue( is_callable( array( 'CoursePress_Data_Instructor', 'added_to_course' ) ) );
+		$this->assertTrue( is_callable( array( 'CoursePress_Data_Instructor', 'remove_from_all_courses' ) ) );
+		$this->assertTrue( is_callable( array( 'CoursePress_Data_Instructor', 'removed_from_course' ) ) );
+		$this->assertTrue( is_callable( array( 'CoursePress_Data_Instructor', 'delete_invitation' ) ) );
+		$this->assertTrue( is_callable( array( 'CoursePress_Data_Instructor', 'send_invitation' ) ) );
+		$this->assertTrue( is_callable( array( 'CoursePress_Data_Instructor', 'create_invite_code_hash' ) ) );
+		$this->assertTrue( is_callable( array( 'CoursePress_Data_Instructor', 'is_course_invite' ) ) );
+		$this->assertTrue( is_callable( array( 'CoursePress_Data_Instructor', 'invitation_data' ) ) );
+		$this->assertTrue( is_callable( array( 'CoursePress_Data_Instructor', 'verify_invitation_code' ) ) );
+		$this->assertTrue( is_callable( array( 'CoursePress_Data_Instructor', 'add_from_invitation' ) ) );
+		$this->assertTrue( is_callable( array( 'CoursePress_Data_Instructor', 'get_students_count' ) ) );
+		$this->assertTrue( is_callable( array( 'CoursePress_Data_Instructor', '_get_students_count' ) ) );
+		$this->assertTrue( is_callable( array( 'CoursePress_Data_Instructor', 'reset_students_count' ) ) );
+		$this->assertTrue( is_callable( array( 'CoursePress_Data_Instructor', 'get_nonce_action' ) ) );
 	}
 
+	public function test_instructor() {
+		$this->assertInstanceOf( 'WP_User', $this->instructor );
+		$this->assertEquals( 'Jon', CoursePress_Data_Instructor::get_first_name( $this->instructor ) );
+		$this->assertEquals( 'Snow', CoursePress_Data_Instructor::get_last_name( $this->instructor ) );
+	}
+
+	public function test_no_courses() {
+		$this->assertEmpty( CoursePress_Data_Instructor::get_course_count( $this->instructor ) );
+		$this->assertEmpty( CoursePress_Data_Instructor::get_course_meta_keys( $this->instructor ) );
+		$this->assertEmpty( CoursePress_Data_Instructor::get_assigned_courses_ids( $this->instructor ) );
+		$this->assertEmpty( CoursePress_Data_Instructor::get_accessable_courses( $this->instructor ) );
+		$statuses = array(
+		   'inherit',
+		   'publish',
+		   'draft',
+		   'future',
+		   'private',
+		);
+		foreach ( $statuses as $post_status ) {
+			$this->assertEmpty( CoursePress_Data_Instructor::get_assigned_courses_ids( $this->instructor, $post_status ) );
+			$this->assertEmpty( CoursePress_Data_Instructor::get_accessable_courses( $this->instructor, $post_status ) );
+		}
+		$this->assertEmpty( CoursePress_Data_Instructor::count_courses( $this->instructor->ID ) );
+	}
+
+	public function test_hash() {
+		CoursePress_Data_Instructor::create_hash( $this->instructor );
+		$hash = CoursePress_Data_Instructor::get_hash( $this->instructor );
+		$this->assertNotEmpty( $hash );
+	}
+
+	public function test_meta_key() {
+		$meta = array( 'meta_key' => 'foo' );
+		$this->assertEquals( 'foo', CoursePress_Data_Instructor::meta_key( $meta ) );
+	}
 }
 
