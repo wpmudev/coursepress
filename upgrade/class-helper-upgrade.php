8facca1ac89846fe43eecf4e512a6a0d6142b88e
Irene <webdevenquiry@gmail.com>
2016-11-02 20:37:02 +0800

    Added 1.x to 2.0 instructor capabilities

--- upgrade/class-helper-upgrade.php
+++ upgrade/class-helper-upgrade.php
@@ -2,7 +2,104 @@
 class CoursePress_Helper_Upgrade {
 	private static $settings = array();
 
+	public static function default_capabilities() {
+		return array(
+			'instructor' => array(
+				/* General */
+				'coursepress_dashboard_cap' => 1,
+				'coursepress_courses_cap' => 1,
+				'coursepress_instructors_cap' => 0, // DEPRECATED
+				'coursepress_students_cap' => 1,
+				'coursepress_assessment_cap' => 1,
+				'coursepress_reports_cap' => 1,
+				'coursepress_notifications_cap' => 1,
+				'coursepress_discussions_cap' => 1,
+				'coursepress_settings_cap' => 1,
+				/* Courses */
+				'coursepress_create_course_cap' => 1,
+				'coursepress_view_others_course_cap' => 1,
+				'coursepress_update_course_cap' => 1,
+				'coursepress_update_my_course_cap' => 1,
+				'coursepress_update_all_courses_cap' => 0, // NOT IMPLEMENTED YET
+				'coursepress_delete_course_cap' => 1,
+				'coursepress_delete_my_course_cap' => 1,
+				'coursepress_delete_all_courses_cap' => 0, // NOT IMPLEMENTED YET
+				'coursepress_change_course_status_cap' => 0,
+				'coursepress_change_my_course_status_cap' => 1,
+				'coursepress_change_all_courses_status_cap' => 0, // NOT IMPLEMENTED YET
+				/* Units */
+				'coursepress_create_course_unit_cap' => 1,
+				'coursepress_view_all_units_cap' => 0,
+				'coursepress_update_course_unit_cap' => 1,
+				'coursepress_update_my_course_unit_cap' => 1,
+				'coursepress_update_all_courses_unit_cap' => 0, // NOT IMPLEMENTED YET
+				'coursepress_delete_course_units_cap' => 1,
+				'coursepress_delete_my_course_units_cap' => 1,
+				'coursepress_delete_all_courses_units_cap' => 0, // NOT IMPLEMENTED YET
+				'coursepress_change_course_unit_status_cap' => 1,
+				'coursepress_change_my_course_unit_status_cap' => 1,
+				'coursepress_change_all_courses_unit_status_cap' => 0, // NOT IMPLEMENTED YET
+				/* Instructors */
+				'coursepress_assign_and_assign_instructor_course_cap' => 0,
+				'coursepress_assign_and_assign_instructor_my_course_cap' => 1,
+				/* Facilitators */
+				'coursepress_assign_my_course_facilitator_cap' => 1,
+				'coursepress_assign_facilitator_cap' => 1,
+				/* Classes */
+				'coursepress_add_new_classes_cap' => 0,
+				'coursepress_add_new_my_classes_cap' => 0,
+				'coursepress_delete_classes_cap' => 0,
+				'coursepress_delete_my_classes_cap' => 0,
+				/* Students */
+				'coursepress_invite_students_cap' => 0,
+				'coursepress_invite_my_students_cap' => 1,
+				'coursepress_withdraw_students_cap' => 0,
+				'coursepress_withdraw_my_students_cap' => 1,
+				'coursepress_add_move_students_cap' => 0,
+				'coursepress_add_move_my_students_cap' => 1,
+				'coursepress_add_move_my_assigned_students_cap' => 1,
+				/* Groups */
+				'coursepress_settings_groups_page_cap' => 0,
+				/* Notifications */
+				'coursepress_create_my_assigned_notification_cap' => 1,
+				'coursepress_create_my_notification_cap' => 1,
+				'coursepress_update_notification_cap' => 0,
+				'coursepress_update_my_notification_cap' => 1,
+				'coursepress_delete_notification_cap' => 0,
+				'coursepress_delete_my_notification_cap' => 1,
+				'coursepress_change_notification_status_cap' => 0,
+				'coursepress_change_my_notification_status_cap' => 1,
+				/* Discussions */
+				'coursepress_create_my_assigned_discussion_cap' => 1,
+				'coursepress_create_my_discussion_cap' => 1,
+				'coursepress_update_discussion_cap' => 0,
+				'coursepress_update_my_discussion_cap' => 1,
+				'coursepress_delete_discussion_cap' => 0,
+				'coursepress_delete_my_discussion_cap' => 1,
+				'coursepress_change_discussion_status_cap' => 0,
+				'coursepress_change_my_discussion_status_cap' => 1,
+				/* Certificates */
+				'coursepress_certificates_cap' => 0,
+				'coursepress_create_certificates_cap' => 0,
+				'coursepress_update_certificates_cap' => 0,
+				'coursepress_delete_certificates_cap' => 0,
+				/* Course Categories */
+				'coursepress_course_categories_manage_terms_cap' => 1,
+				'coursepress_course_categories_edit_terms_cap' => 1,
+				'coursepress_course_categories_delete_terms_cap' => 0,
+				/* Posts and Pages */
+				'edit_pages' => 0,
+				'edit_published_pages' => 0,
+				'edit_posts' => 0,
+				'publish_pages' => 0,
+				'publish_posts' => 0,
+			),
+		);
+	}
+
 	public static function update_settings() {
+		$default_capabilities = self::default_capabilities();
+
 		$settings = array(
 			'slugs' => array(
 				'course' => get_option( 'coursepress_course_slug', 'courses' ),
@@ -35,6 +132,7 @@ class CoursePress_Helper_Upgrade {
 			),
 			'instructor' => array(
 				'use_username' => get_option( 'show_instructor_username', 1 ),
+				'capabilities' => get_option( 'coursepress_instructor_capabilities', $default_capabilities ),
 			),
 			'course' => array(
 				'details_media_type' => get_option( 'details_media_type', 'default' ),
