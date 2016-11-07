181487a4474d5aa265751d8c188fb5b899c7553f
Irene <webdevenquiry@gmail.com>
2016-11-07 17:03:31 +0800

    Try submodule absolute path

--- tests/php/test-upgrade.php
+++ tests/php/test-upgrade.php
@@ -4,6 +4,6 @@
  **/
 class CoursePressUpgradeTest extends WP_UnitTestCase {
 	public function testVersionSwitch() {
-		$this->assertThat( CoursePressUpgrade::check_old_courses(), true, $this->assertTrue( '1.x', CoursePressUpgrade::$current_version ) );
+		$this->assertThat( CoursePressUpgrade::check_old_courses(), true, $this->assertTrue( '1.x', CoursePressUpgrade::$coursepress_version ) );
 	}
 }
