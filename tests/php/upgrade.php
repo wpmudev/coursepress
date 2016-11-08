298b9da9d34109fcde3f4504e17ab692f8b75872
Irene <webdevenquiry@gmail.com>
2016-11-08 22:17:31 +0800

    Test version switch

--- tests/php/upgrade.php
+++ tests/php/upgrade.php
@@ -4,6 +4,24 @@
  **/
 
 class CoursePressUpgradeTest extends WP_UnitTestCase {
+	public function test_version_switch() {
+		$coursepress = WP_COURSEPRESS_DIR . 'coursepress.php';
+
+		require $coursepress;
+
+		// Expect 2.0
+		$this->assertEquals( false, CoursePressUpgrade::check_old_courses() );
+
+		$c1 = self::factory()->post->create(array(
+			'post_type' => 'course',
+			'post_title' => 'Course 1',
+		));
+
+		// Expect 1.x
+		$this->assertEquals( true, CoursePressUpgrade::check_old_courses() );
+	}
+
+/*
 
 	public function test_load_version_1() {
 		$upgrade_class = WP_COURSEPRESS_DIR . 'upgrade/class-upgrade.php';
@@ -31,4 +49,14 @@ class CoursePressUpgradeTest extends WP_UnitTestCase {
 		$instance->plugin_dir = WP_PLUGIN_DIR . '/coursepress/1.x/';
 		$instance->plugin_url = WP_PLUGIN_URL . '/coursepress/1.x/';
 	}
+
+	public function test_load_version_2() {
+		$version_2 = WP_COURSEPRESS_DIR . '2.0/';
+		$version_file = $version_2 . 'coursepress.php';
+
+		require $version_file;
+
+		$this->assertTrue( class_exists( 'CoursePress' ) );
+	}
+*/
 }
