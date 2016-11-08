574d2fd9516737f118b106cb6b0a11c3f28c1883
Irene <webdevenquiry@gmail.com>
2016-11-08 18:58:27 +0800

    Set dummy vars

--- tests/php/upgrade.php
+++ tests/php/upgrade.php
@@ -16,8 +16,9 @@ class CoursePressUpgradeTest extends WP_UnitTestCase {
 		$version_1 = WP_COURSEPRESS_DIR . '1.x/';
 		$version_file = $version_1 . 'coursepress.php';
 
-		if ( ! defined( 'WP_PLUGIN_DIR' ) && function_exists( 'wp_plugin_directory_constants' ) ) {
-			wp_plugin_directory_constants();
+		if ( ! defined( 'WP_PLUGIN_DIR' ) ) {
+			define( 'WP_PLUGIN_DIR', dirname( WP_COURSEPRESS_DIR ) );
+			define( 'WP_PLUGIN_URL', false ); // @todo: Get the correct value
 		}
 
 		require $version_file;
