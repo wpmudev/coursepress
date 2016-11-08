66ae7baab723b5707bfc3cd338acd3d1a8d5cc87
Irene <webdevenquiry@gmail.com>
2016-11-08 19:35:07 +0800

    Test require

--- tests/php/upgrade.php
+++ tests/php/upgrade.php
@@ -4,22 +4,19 @@
  **/
 
 class CoursePressUpgradeTest extends WP_UnitTestCase {
-	public static function load_version_1() {
+	/**
+	 * @requires function wp_plugin_directory_constants
+	 **/
+	public static function test_load_version_1() {
 		$upgrade_class = WP_COURSEPRESS_DIR . 'upgrade/class-upgrade.php';
 
 		// Load upgrade class
 		require $upgrade_class;
-		//CoursePress_Upgrade::init();
 
 		add_action( 'coursepress_init_vars', array( __CLASS__, 'init_vars' ) );
 
 		$version_1 = WP_COURSEPRESS_DIR . '1.x/';
 		$version_file = $version_1 . 'coursepress.php';
-
-		if ( ! defined( 'WP_PLUGIN_DIR' ) ) {
-			define( 'WP_PLUGIN_DIR', dirname( WP_COURSEPRESS_DIR ) );
-			define( 'WP_PLUGIN_URL', false ); // @todo: Get the correct value
-		}
 
 		require $version_file;
 	}
@@ -30,9 +27,10 @@ class CoursePressUpgradeTest extends WP_UnitTestCase {
 		$instance->plugin_url = WP_PLUGIN_URL . '/coursepress/1.x/';
 	}
 
+	/**
+	 * @depends test_load_version_1
+	 **/
 	public function testVersion1() {
-		self::load_version_1();
-
 		$this->assertTrue( class_exists( 'CoursePress' ) );
 	}
 }
