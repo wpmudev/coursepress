f8406ba33f576b08e1a6f97332e4a02df5416c28
Irene <webdevenquiry@gmail.com>
2016-11-09 20:22:24 +0800

    Fix bootstrap

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,7 +1,7 @@
 <?php
 ob_start();
 // Set coursepress main location
-
+define( 'WP_COURSEPRESS_DIR', dirname( __DIR__ ) . '/' );
 $_tests_dir = getenv( 'WP_TESTS_DIR' );
 
 // Setup environment
@@ -18,12 +18,10 @@ if ( ! $_tests_dir ) {
 
 if ( ! file_exists( $_tests_dir ) ) {
 	// Got problem with test dir location, coursepress in 6 level deep
-	$_tests_dir = dirname( dirname( dirname( dirname( dirname( __FILE__ ) ) ) ) ) . '/tests/phpunit';
+	$_tests_dir = dirname( dirname( dirname( dirname( WP_COURSEPRESS_DIR ) ) ) ) . '/tests/phpunit';
 }
 
 define( 'WP_TESTS_DIR', $_tests_dir );
 
 // Load tests bootstrap
 require WP_TESTS_DIR . '/includes/bootstrap.php';
-
-define( 'WP_COURSEPRESS_DIR', WP_PLUGIN_DIR . '/coursepress/' );
