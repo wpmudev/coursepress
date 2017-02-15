2c6669d0f7d593a2dd48db48030a4defd4c29eb4
Irene <webdevenquiry@gmail.com>
2017-02-15 10:48:13 +0800

    Build 2.0.4

--- upgrade/js/admin-upgrade.js
+++ upgrade/js/admin-upgrade.js
@@ -1,6 +1,6 @@
-/*!  - v2.0.0
+/*!  - v2.0.4
  * https://premium.wpmudev.org/project/coursepress-pro/
- * Copyright (c) 2016; * Licensed GPLv2+ */
+ * Copyright (c) 2017; * Licensed GPLv2+ */
 _.extend( _coursepress_upgrade, {
 	totalCourses: 0,
 	totalSuccess: 0,
