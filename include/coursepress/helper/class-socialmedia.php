cfeb973d79335cdceef0a28ef897e811ad8f932b
Marcin Pietrzak <marcin@iworks.pl>
2017-02-20 09:57:07 +0100

    CoursePress Pro Social Sharing Functions https://app.asana.com/0/165990678494723/276004344288767 https://premium.wpmudev.org/forums/topic/coursepress-pro-social-sharing-functions I request a feature to have a setting to turn on/off built-in social sharing. I have a commercial plugin that does social sharing and don't want to use the built-in option.

---
+++ include/coursepress/helper/class-socialmedia.php
@@ -0,0 +1,42 @@
+<?php
+
+class CoursePress_Helper_SocialMedia {
+	/**
+	 * Function all to get Social Sharing services with labels.
+	 *
+	 * @since 2.0.5
+	 *
+	 * @return array Array of social services.
+	 */
+	public static function get_social_sharing_array() {
+		$services = array(
+			'facebook' => __( 'Facebook', 'CP_TD' ),
+			'twitter' => __( 'Twitter', 'CP_TD' ),
+			'google' => __( 'Google', 'CP_TD' ),
+			'email' => __( 'Email', 'CP_TD' ),
+		);
+		/**
+		 * Filter allow to add some social media.
+		 *
+		 * Filter allow to add some social media. You need to use
+		 * "coursepress_social_link_{$service}" filter to handle frontend output.
+		 *
+		 * @see CoursePress_Data_Shortcode_CourseTemplatecourse_social_links()
+		 *
+		 * @since 2.0.5
+		 */
+		return apply_filters( 'coursepress_social_media_social_sharing_array', $services );
+	}
+	/**
+	 * Function all to get Social Sharing services keys.
+	 *
+	 * @since 2.0.5
+	 *
+	 * @return array Array of social services keys.
+	 */
+	public static function get_social_sharing_keys() {
+		$services = self::get_social_sharing_array();
+		$services = array_keys( $services );
+		return $services;
+	}
+}
