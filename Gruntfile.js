2c44a5fc87fbcfaeee21b542df426171592c542a
Joel James <me@joelsays.com>
2017-09-13 22:53:55 +0530

    Added rtl file

--- Gruntfile.js
+++ Gruntfile.js
@@ -56,6 +56,7 @@ module.exports = function(grunt) {
 			'{css}coursepress_front.css': '{css}src/coursepress_front.scss',
 			'{css}bbm.modal.css':         '{css}src/bbm.modal.scss',
 			'{css}editor.css':            '{css}src/editor.scss',
+			'{css}editor-rtl.css':        '{css}src/editor.scss',
 			'{css}admin-ui.css':          '{css}src/admin/admin-ui.scss',
 			'{css}front.css':             '{css}src/front.scss',
 			'{css}admin-menu.css':		   '{css}src/admin-menu.scss'
