ec9a4d643d1066f73d8e3e53eea81bada2b42b42
Hassan Akhtar <hasan.yo41@gmail.com>
2017-10-22 19:41:44 +0100

    Changed the search query parameter on instructors page

--- views/admin/instructors.php
+++ views/admin/instructors.php
@@ -34,7 +34,7 @@ foreach ( $courses as $course_id => $course ) {
                     <label class="label"><?php _e( 'Search instructors by name, username or email', 'cp' ); ?></label>
                     <div class="cp-input-clear">
                         <input type="hidden" name="page" value="<?php echo esc_attr( $page ); ?>" />
-                        <input type="text" name="s" placeholder="<?php _e( 'Type here...', 'cp' ); ?>" value="<?php echo $search; ?>" />
+                        <input type="text" name="instructor_search" placeholder="<?php _e( 'Type here...', 'cp' ); ?>" value="<?php echo $search; ?>" />
                         <button type="button" id="cp-search-clear" class="cp-btn-clear"><?php _e( 'Clear', 'cp' ); ?></button>
                     </div>
                     <button type="submit" class="cp-btn cp-btn-active"><?php _e( 'Search', 'cp' ); ?></button>
