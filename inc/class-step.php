b53716fd904e66d1be44e2c7b2b7fbd7482fe7cc
Hassan Akhtar <hasan.yo41@gmail.com>
2017-12-27 08:50:37 +0500

    Replaced hardcoded values and made sure model is updated when preview status changed

--- inc/class-step.php
+++ inc/class-step.php
@@ -50,6 +50,7 @@ class CoursePress_Step extends CoursePress_Unit {
 			'module_page',
 			'show_content',
 			'allowed_file_types',
+			'preview'
 		);
 
 		return $keys;
@@ -78,8 +79,6 @@ class CoursePress_Step extends CoursePress_Unit {
 			$this->__set( $key, $value );
 			$this->__set( 'meta_' . $key, $value );
 		}
-
-		$this->__set( 'preview', true );
 	}
 
 	function get_settings() {
