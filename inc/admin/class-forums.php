f0e42057fb8a19594f1c35be0b198b9a05ba56f8
ioannup <ioannup@gmail.com>
2018-03-10 14:06:34 +0200

    Fix for saving html-tags for Forum description

--- inc/admin/class-forums.php
+++ inc/admin/class-forums.php
@@ -202,7 +202,7 @@ class CoursePress_Admin_Forums extends CoursePress_Admin_Page {
 			'ID' => $_POST[ $this->id_name ],
 			'post_title' => sanitize_text_field( isset( $_POST['post_title'] )? $_POST['post_title']:'' ),
 			'post_parent' => intval( isset( $_POST['course_id'] )? $_POST['course_id']:0 ),
-			'post_content' => sanitize_textarea_field( isset( $_POST['post_content'] )? $_POST['post_content']:'' ),
+			'post_content' => isset( $_POST['post_content'] )? $_POST['post_content']:'',
 			'post_type' => $this->post_type,
 			'post_status' => 'publish',
 			'meta_input' => array(
