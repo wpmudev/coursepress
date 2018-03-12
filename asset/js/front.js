10db3be6bc3ed9e18107a59d57276342e10b9271
Marcin Pietrzak <marcin@iworks.pl>
2018-03-12 07:48:08 +0100

    prepare 2.1.5

--- asset/js/front.js
+++ asset/js/front.js
@@ -1,4 +1,4 @@
-/*! CoursePress - v2.1.5-beta.1
+/*! CoursePress - v2.1.5
  * https://premium.wpmudev.org/project/coursepress-pro/
  * Copyright (c) 2018; * Licensed GPLv2+ */
 var CoursePress = {};
