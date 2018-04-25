2f0a760ecf939d644f6f41aa248268caf3872115
Racase Inc <rakesh.lawaju@incsub.com>
2018-04-25 17:03:21 +0545

    Check if user have already commented on step or not

--- inc/step/class-discussion.php
+++ inc/step/class-discussion.php
@@ -9,9 +9,16 @@ class CoursePress_Step_Discussion extends CoursePress_Step {
 	protected $type = 'discussion';
 
 	public function validate_response( $response = array() ) {
-		$request = $_POST;
+		$request  = $_POST;
+		$step_id  = $request['step_id'];
+		$user_id  = get_current_user_id();
+		$comments = get_comments( array(
+			'user_id' => $user_id,
+			'post_id' => $step_id,
+		) );
 
-		if ( $this->is_required() && empty( $request['comment'] ) ) {
+		$count = count( $comments );
+		if ( $this->is_required() && empty( $request['comment'] ) && 1 > $count ) {
 			// Redirect back.
 			$referer = filter_input( INPUT_POST, 'referer_url' );
 			$error   = __( 'Response is required for all fields.', 'cp' );
