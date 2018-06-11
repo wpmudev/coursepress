3898f931c8407c61ca4e5dee91890d25c55c3c49
Marcin Pietrzak <marcin@iworks.pl>
2018-06-11 14:22:18 +0200

    tablet

--- assets/js/src/front/fold-unit.js
+++ assets/js/src/front/fold-unit.js
@@ -1,29 +1,36 @@
-/* global CoursePress */
+/* global CoursePress, _coursepress */
 
 (function() {
     'use strict';
-
     CoursePress.Define( 'FoldCourseUnits', function( $, doc ) {
-
-      var foldMenu,foldButton,target;
-
-      foldButton = $( '<span class="fold"></span>' ).insertBefore( $('.unit') );
-
-      foldMenu = function() {
-
-        target = $(this).next().find('.module-tree');
-
-        $(this).toggleClass('folded');
-
-        if ($(this).is( '.folded' )) {
-          target.slideUp();
-        } else {
-          target.slideDown();
-        }
-
-      };
-
-      $(doc).on( 'click', '.fold', foldMenu );
-
+        var foldMenu,foldButton,target, toggleButton, toggleMenu;
+        /**
+         * fold
+         */
+        foldButton = $( '<span class="fold"></span>' ).insertBefore( $('.unit') );
+        foldMenu = function() {
+            target = $(this).next().find('.module-tree');
+            $(this).toggleClass('folded');
+            if ($(this).is( '.folded' )) {
+                target.slideUp();
+            } else {
+                target.slideDown();
+            }
+        };
+        $(doc).on( 'click', '.fold', foldMenu );
+        /**
+         * toggle
+         */
+        toggleButton = $( '<span class="toggle">'+_coursepress.text.shortcodes.unit_archive_list.unfold+'</span>' ).insertBefore( $('.course-structure') );
+        toggleMenu = function() {
+            if ( $(this).hasClass('open') ) {
+                $('.course-structure').hide();
+                $(this).removeClass('open').html( _coursepress.text.shortcodes.unit_archive_list.unfold);
+            } else {
+                $('.course-structure').show();
+                $(this).addClass('open').html( _coursepress.text.shortcodes.unit_archive_list.fold);
+            }
+        };
+        $(doc).on( 'click', '.toggle', toggleMenu);
     });
 })();
